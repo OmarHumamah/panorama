88a9c1ea06a74823429777904df9450941e77725
OmarHumamah <>
2021-06-02 20:27:22 +0000

    some fixees

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ if(answer == "yes"){alert ("Then you are the rghit place.")}
 else if(answer == "no"){alert ("You will like it here.")}
 
 else{alert("Try to discoer our page.")}
+
 console.log(answer)
 
 document.write("Visit us again Soon!")
