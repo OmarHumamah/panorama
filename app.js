5aaaec0e8ecf774cd77dd68c89adacf4af054762
OmarHumamah <>
2021-06-02 13:36:02 +0000

    jsfix

---
+++ app.js
@@ -0,0 +1,12 @@
+alert ( "Welcom to Panorama, we glad you are hear." )
+
+var answer = prompt ("Do you like photograph.")
+
+if(answer == "yes"){alert ("Then you are the rghit place.")}
+
+else if(answer == "no"){alert ("You will like it here.")}
+
+else{alert("Try to discoer our page.")}
+console.log(answer)
+
+document.write("Visit us again Soon!")
