61d3d23c42cc48e80246524657a1fd1c904a1cc4
OmarHumamah <>
2021-06-03 14:57:40 +0000

    add photo

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-alert ( "Welcom to Panorama, we glad you are hear." )
+/* alert("Welcom to Panorama, we glad you are hear.")
 
-var answer = prompt ("Do you like photograph.")
+var age = prompt ("Are you +18?" + '\xa0\xa0\xa0' + "type your age.")
 
-if(answer == "yes"){alert ("Then you are the rghit place.")}
+console.log(age)
+while((age) <= 18 ){
+var age = prompt ("Are you +18? type your age.")
+}
 
-else if(answer == "no"){alert ("You will like it here.")}
+var answer = prompt("Do you like photograph.")
 
-else{alert("Try to discoer our page.")}
+if (answer == "yes") { alert("Then you are the rghit place.") }
+
+else if (answer == "no") { alert("You will like it here.") }
+
+else { alert("Try to discoer our page.") }
 
 console.log(answer)
+*/
+
+var exi = prompt("How excited you are from 1 - 10?" )
+var num = parseInt(exi, 3);
+console.log(num)
+for (var i=0; i<num ;i++){
+document.getElementById('star-'+i).style.display='block';
+}
 
 document.write("Visit us again Soon!")
