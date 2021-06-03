175ae42f00f5184168b6101f880022203768c3b5
OmarHumamah <>
2021-06-03 16:23:09 +0000

    fix star1

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-/* alert("Welcom to Panorama, we glad you are hear.")
+alert("Welcom to Panorama, we glad you are hear.")
 
 var age = prompt ("Are you +18?" + '\xa0\xa0\xa0' + "type your age.")
 
 console.log(age)
-while((age) <= 18 ){
+while((age) < 18 ){
 var age = prompt ("Are you +18? type your age.")
 }
 
-var answer = prompt("Do you like photograph.")
+/* var answer = prompt("Do you like photograph.")
 
 if (answer == "yes") { alert("Then you are the rghit place.") }
 
@@ -15,14 +15,14 @@ else if (answer == "no") { alert("You will like it here.") }
 
 else { alert("Try to discoer our page.") }
 
-console.log(answer)
-*/
+console.log(answer) */
 
-var exi = prompt("How excited you are from 1 - 10?" )
-var num = parseInt(exi, 3);
-console.log(num)
-for (var i=0; i<num ;i++){
-document.getElementById('star-'+i).style.display='block';
+
+var exi = prompt("How excited you are from 1 - 5?" )
+var num = parseInt(exi, 6);
+
+for (var i=1; i<=num ;i++){
+document.getElementById('star'+i).style.display='inline-block';
 }
 
 document.write("Visit us again Soon!")
