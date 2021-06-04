881b7f2e187739ff99cd57ff5dc5e50588bf8719
OmarHumamah <>
2021-06-04 09:25:01 +0000

    css-js01

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
 alert("Welcom to Panorama, we glad you are hear.")
 
-var age = prompt ("Are you +18?" + '\xa0\xa0\xa0' + "type your age.")
 
-console.log(age)
-while((age) < 18 ){
+var age = prompt ("Are you +18?" + '\xa0\xa0\xa0' + "type your age.") 
+
+
+
+while( age < 18 ){
 var age = prompt ("Are you +18? type your age.")
+
 }
 
-/* var answer = prompt("Do you like photograph.")
 
-if (answer == "yes") { alert("Then you are the rghit place.") }
 
-else if (answer == "no") { alert("You will like it here.") }
 
-else { alert("Try to discoer our page.") }
+// /* var answer = prompt("Do you like photograph.")
 
-console.log(answer) */
+// if (answer == "yes") { alert("Then you are the rghit place.") }
+
+// else if (answer == "no") { alert("You will like it here.") }
+
+// else { alert("Try to discoer our page.") }
+
+// console.log(answer) */
 
 
 var exi = prompt("How excited you are from 1 - 5?" )
 var num = parseInt(exi, 6);
 
+while(exi > 5){var exi = prompt("How excited you are from 1 - 5?" + '\xa0\xa0\xa0' + "Less than 5 please!" )
+var num = parseInt(exi, 6);}
+
 for (var i=1; i<=num ;i++){
-document.getElementById('star'+i).style.display='inline-block';
+document.getElementById('star'+i).style.display='inline';
 }
 
 document.write("Visit us again Soon!")
+
+// var x = prompt("num")
+// x = n
+
